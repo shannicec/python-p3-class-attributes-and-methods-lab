eea264fddc50a2b12cb8636a2825ce87d9c68a8c
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-10-06 19:04:18 +0300

    added the code for the tests to pass

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,57 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment the song count
+        self.add_song_to_count()
+
+        # Add artist and genre
+        self.add_to_artists(self.artist)  
+        self.add_to_genres(self.genre)    
+
+        # Add to artist and genre counts
+        self.add_to_genre_count(self.genre)
+        self.add_to_artist_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        """Increments the song count."""
+        cls.count += 1
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Adds unique artists to the artists list."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Adds unique genres to the genres list."""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Adds to the genre count histogram."""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        """Adds to the artist count histogram."""
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
